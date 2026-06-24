4ceece2af4d18f1d7987a512ce001d1d8f027786
Pankaj Kumar <pankajk@clavax.com>
2026-06-24 14:11:31 +0530

    Install EFcore from nuget and configure in Program .cs

---
+++ DbOperationWithEFcore/Data/appDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationWithEFcore.Data
+{
+    public class appDbContext : DbContext
+    {
+        public appDbContext(DbContextOptions<appDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
